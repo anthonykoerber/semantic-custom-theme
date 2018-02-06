c583cf22ae7806faf09183e972e11cc1eb79ca0a
Anthony Koerber <akoerber@mediamath.com>
2018-02-06 16:17:55 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Button } from "semantic-ui-react";
+
+import "semantic-ui-less/semantic.less";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button>Hello There</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
